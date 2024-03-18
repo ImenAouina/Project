3704a6547e3622eb5434a46485f44ac51d65066d
Helmi Touati <touatizh@gmail.com>
2024-03-19 00:07:25 +0100

    Add AuthController for user authentication
    - Implemented `AuthController` to handle user registration, login, and logout
    - Included model-level validation for user registration and login forms
    - Validated email uniqueness during user registration
    - Hashed user passwords before storing them in the database
    - Stored user session using `HttpContext.Session`
    - Added `LoginUser` model for representing login form data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using chore_tracker.Models;
 
 namespace chore_tracker.Controllers;
@@ -7,14 +10,19 @@ namespace chore_tracker.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly Context _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, Context context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        if (!HttpContext.Session.GetInt32("UserId").HasValue)
+            return RedirectToAction("Index", "Auth");
+
         return View();
     }
 
