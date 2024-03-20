d658ebb702820057f4c06448b1b29c487c2f0e10
unknown <aouinaimen@gmail.com>
2024-03-20 23:44:50 +0100

    Dashboard

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,13 +17,71 @@ public class HomeController : Controller
         _logger = logger;
         _context = context;
     }
-
+    
+    //Dashboard//
+    //[HttpGet("dashboard")]
     public IActionResult Index()
     {
         if (!HttpContext.Session.GetInt32("UserId").HasValue)
+        {
             return RedirectToAction("Index", "Auth");
+        }
+        List<Job> AllJobs = _context.Jobs
+        .Include(j => j.Creator)
+        .Include(j=> j.UserJobs)
+        .ToList();
+        return View(AllJobs);
+    }
+    //Delete Job //
+    public IActionResult DeleteJob(int jobId)
+    {
+        Job? JobToRemove = _context.Jobs.SingleOrDefault(j=> j.JobId == jobId);
+        _context.Jobs.Remove(JobToRemove);
+        _context.SaveChanges();
+        return RedirectToAction("Index","Home");
+    }
+    //Add to my Jobs
+     public IActionResult AddToMyJobs(int jobId)
+    {
+         if (!HttpContext.Session.GetInt32("UserId").HasValue)
+        {
+            return RedirectToAction("Index", "Auth");
+        }
+        UserJob newUserJob = new UserJob
+        {
+            UserId = (int)HttpContext.Session.GetInt32("UserId"),
+            JobId = jobId
+        };
+            _context.UserJobs.Add(newUserJob);
+            _context.SaveChanges();
+            return RedirectToAction("Index");   
+    }
 
-        return View();
+    //Add Job 
+    [HttpGet("addJob")]
+    public IActionResult AddJob()
+    {
+        if (!HttpContext.Session.GetInt32("UserId").HasValue)
+        {
+            return RedirectToAction("Index", "Auth");
+        }
+        return View("AddJob");
+    }
+
+    [HttpPost]
+    public IActionResult CreateJob(Job newJob)
+    {
+        if (!HttpContext.Session.GetInt32("UserId").HasValue)
+        {
+            return RedirectToAction("Index", "Auth");
+        }
+        if (ModelState.IsValid)
+        {
+            _context.Jobs.Add(newJob);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View("AddJob");
     }
 
     public IActionResult Privacy()
